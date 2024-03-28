58f27c2068f7857b32eec329109ff1ca8ee57542
Juno <anint3647@gmail.com>
2024-03-28 17:27:30 +0900

    🩹 : eslint prettier 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyles";
+import { Outlet } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyles';
 
 function App() {
   return (
