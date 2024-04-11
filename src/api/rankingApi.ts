6f725dade16702265d5ba4fdfbc55f41445a3fab
김기현 <kim724265@gmail.com>
2024-04-11 14:49:06 +0900

    ♻️ Ranking 정보 가져오는 로직 useQuery 사용해서 리팩토링

--- src/api/rankingApi.ts
+++ src/api/rankingApi.ts
@@ -2,7 +2,7 @@ import { authInstance } from './api';
 
 export const fetchRankings = async () => {
   try {
-    const res = await authInstance.get('https://friendj.store/top-likes');
+    const res = await authInstance.get('/top-likes');
     return res.data;
   } catch (err) {
     console.log('err', err);
