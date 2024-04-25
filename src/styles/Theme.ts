3953df02e0924bb80a34bd3166c3b8f336568ce1
김기현 <kim724265@gmail.com>
2024-04-25 10:09:46 +0900

    ✨다크모드 Zustand 스토어 생성

---
+++ src/styles/Theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  bgImage: 'url',
+  fontColor: '#000',
+  ModalBox: '#fff',
+  StateModal: '#fff',
+  footerArea: '#d7e5fa',
+  button: '#b0b0b0',
+};
+
+export const darkTheme = {
+  bgImage: 'url',
+  fontColor: '#fff',
+  ModalBox: '#121212',
+  StateModal: '#121212',
+  footerArea: '#2f4768',
+  button: '#fff',
+};
