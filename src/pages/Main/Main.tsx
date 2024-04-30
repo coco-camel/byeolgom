a1afe814e61b8f534fa8e71d43e77a0e767ed2f5
김기현 <kim724265@gmail.com>
2024-04-30 10:42:12 +0900

    🐛 refresh Token 사용해서 access Token 재발급 로직 수정

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,10 @@
 import PostArrived from '../../components/postArrived/PostArrived.tsx';
 import MyElement3D from '../../components/3dMotion/MyElement3D.tsx';
 import MainPageButton from '../../components/button/MainPageButton.tsx';
+import { refreshAccessToken } from '../../api/api';
 
 function Main() {
+  refreshAccessToken();
   return (
     <>
       <PostArrived />
